80db3f5da59f1e0bf48f0d59bfd39a18b2cf424e
Android <android@Rnc505-2.local>
2011-11-03 23:20:47 -0400

    Initial Commit

---
+++ DodgeIt/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  DodgeIt
+//
+//  Created by Android on 11/3/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ DodgeIt/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DodgeIt
+//
+//  Created by Android on 11/3/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
